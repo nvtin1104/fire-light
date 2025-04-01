1418b371be97ef2d5d92fd83fbc3c47a27f21736
Nguyễn Văn Tín <bguyt46@gmail.com>
2025-04-01 21:13:27 +0700

    build: sửa config

--- client/next.config.js
+++ client/next.config.js
@@ -1,10 +1,6 @@
-
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
+@type {import('next').NextConfig} 
 
 const nextConfig = {
-  basePath: isProd ? "/Desgy" : "",
-  assetPrefix: isProd ? "/Desgy/" : "",
   output: "export",
   images: {
     unoptimized: true,
